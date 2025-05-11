f7d55e3592787aedefe111a338573592cf9ea63c
Rahul Samant <161972011+rahulsamant37@users.noreply.github.com>
2025-05-11 16:19:50 +0000

    feat: replace search bar UI with language input fields

---
+++ src/components/Inputs/FileUpload.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { IconPaperclip } from "@tabler/icons-react";
+
+const FileUpload = ({ handleFileUpload }) => (
+  <label htmlFor="file-upload" className="cursor-pointer">
+    <IconPaperclip size={21} />
+    <input
+      type="file"
+      id="file-upload"
+      onChange={handleFileUpload}
+      className="hidden"
+    />
+  </label>
+);
+
+export default FileUpload;
